38956c842e2d50835c139f0ecb0ec96e3e81b836
JaimeLlanos <jaime.jlm@hotmail.com>
2018-04-09 16:22:36 +0200

    Proyecto con el "lanzador"

---
+++ practicaPL/src/Analizador.java
@@ -0,0 +1,26 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+/**
+*/
+public class Analizador{
+	public static void main(String argv[]){
+		String a = "C:\\Users\\jaime\\workspace\\practicaPL\\src\\correcto2.txt";
+				AnalizadorLexico lexico = null;
+				try {
+					lexico = new AnalizadorLexico( new java.io.FileReader(a));
+					parser sintactico = new parser(lexico);
+					sintactico.parse();
+				}
+				catch (java.io.FileNotFoundException e) {
+					System.out.println("Archivo \""+a+"\" no encontrado.");
+				}
+				catch (java.io.IOException e) {
+					System.out.println("Error durante la lectura del"+" archivo \""+a+"\".");
+					e.printStackTrace();
+				}
+				catch (Exception e) {
+					System.out.println("Excepcion:");
+					e.printStackTrace();
+				}
+			}
+		}
